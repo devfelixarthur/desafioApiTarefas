3c9f44e139964a08d0d988d701476c378a20522f
Arthur Felix <arthur.andrade@omnilink.com.br>
2024-09-10 21:28:41 -0300

    Inicializando o projeto

---
+++ tarefas/src/main/java/com/api/v1/tarefas/GerenciamentoDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.api.v1.tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeTarefasApplication.class, args);
+	}
+
+}
---
+++ tarefas/src/test/java/com/api/v1/tarefas/GerenciamentoDeTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.v1.tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
